46e9fd67b853c99640c7fe4c81743f2b4e97c34f
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-06-16 10:28:02 +0200

    - FIX: icalconv

--- helper/icalconv.py
+++ helper/icalconv.py
@@ -8,8 +8,8 @@ def getExchangeId(ical_uid):
     c = conn.cursor()
 
     r = c.execute(
-        'select soap_id, soap_chkey from items where icalendar_id = ?',
-        ical_uid)
+        "select soap_id, soap_chkey from items where icalendar_id=?",
+        (ical_uid,))
 
     row = r.fetchone()
 
@@ -32,24 +32,24 @@ def setExchangeId(ical_uid, exchangeId):
 
     r = c.execute(
         'insert exchange_id, soap_id, soap_chkey into items values (?,?,?)',
-        ical_uid, ex_id, ex_chkey)
+        (ical_uid, ex_id, ex_chkey))
 
     conn.commit()
     conn.close()
               
 
-def getItemId(icalendar_id=None,exchange_id=None,fallback=None)
+def getItemId(icalendar_id=None,exchange_id=None,fallback=None):
     conn = sqlite3.connect(db)
     c = conn.cursor()
 
     if icalendar_id:
-        r = c.execute('select id from items where icalendar_id = ?', icalendar_id)
+        r = c.execute('select id from items where icalendar_id = ?', (icalendar_id,))
         item_id = fetchone()
         if item_id: item_id = item_id[0]
     elif exchange_id:
         ex_id, ex_chkey = exchange_id
         r = c.execute('select id from items where soap_id = ? and soap_chkey = ?',
-                      ex_id, ex_chkey)
+                      (ex_id, ex_chkey))
         item_id = fetchone()
         if item_id: item_id = item_id[0]
 
@@ -69,14 +69,14 @@ def getICalFields(item_id=None,icalendar_id=None,exchange_id=None):
 
     r = c.execute(
         'select icalendar_field, icalendar_value from icalendar_fields where parent_id = ?',
-        item_id)
+        (item_id,))
 
     conn.commit()
     conn.close()
     return r.fetchall()
     
 
-def setICalFields(item_id=None,icalendar_id=None,exchange_id=None,fields):
+def setICalFields(item_id=None,icalendar_id=None,exchange_id=None,fields=[]):
 
     conn = sqlite3.connect(db)
     c = conn.cursor()
@@ -85,12 +85,12 @@ def setICalFields(item_id=None,icalendar_id=None,exchange_id=None,fields):
 
     if not item_id: return None
 
-    r = c.execute('delete from icalendar_fields where parent_id = ?', item_id)
+    r = c.execute('delete from icalendar_fields where parent_id = ?', (item_id,))
 
     for k,v in fields:
         r = c.execute(
-            'insert into icalendar_fields (parent_id,icalendar_field,icalendar_value)
-             values (?,?,?)', item_id, k, v);
+            "insert into icalendar_fields (parent_id,icalendar_field,icalendar_value) (?,?,?)",
+            (item_id, k, v));
 
     conn.commit()
     conn.close()

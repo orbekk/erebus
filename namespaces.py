38b148fdad09e08126e6990185d16b00d480dab3
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-06-18 11:04:51 +0200

    - Added no_namespace-metod

--- namespaces.py
+++ namespaces.py
@@ -7,5 +7,13 @@ def m(s):
 def t(s):
     return with_ns(types, s)
 
+def no_namespace(s):
+    arr = s.split('}')
+    if len(arr) == 2:
+        return arr[1]
+    else:
+        return None
+
 types    = "http://schemas.microsoft.com/exchange/services/2006/types"
 messages = "http://schemas.microsoft.com/exchange/services/2006/messages"
+

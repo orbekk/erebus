099666914757c85a521243572fba4b467bf3527a
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-06-30 12:15:50 +0200

    Added missing exception

--- CNodeVisitor.py
+++ CNodeVisitor.py
@@ -1,8 +1,16 @@
+# -*- coding: utf8 -*-
 from CNode import *
+
+class MissingVisitor(Exception):
+    def __init__(self, visitor):
+        self.msg = str
+
+    def __str__(self):
+        return "Visitor %s missing" % self.visitor
 
 class CNodeVisitor(object):
     def visit(self,obj,*args,**kws):
-        method_name = "visit%s" % obj.__class__.__name__
+        method_name = "visit%s" % obj.name
 
         if hasattr(self, method_name):
             method = getattr(self, method_name)
@@ -11,7 +19,7 @@ class CNodeVisitor(object):
             if hasattr(self, 'visit_any'):
                 return self.visit_any(obj,*args,**kws)
             else:
-                raise MissingVisitor("visit%s" % obj.__class__.__name__)
+                raise MissingVisitor("visit%s" % method_name)
 
     def accept(self,obj,attr):
         """Call this to visit children with name 'attr'"""

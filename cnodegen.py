7877fbccc4936e949a302274b679c94568719646
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-06-30 09:22:35 +0200

    added conversion to CNode from ics and xml

---
+++ cnodegen.py
@@ -0,0 +1,33 @@
+from icalendar import Calendar as ICal
+from icalendar.cal import Component
+from CNode import *
+from CNode2StringVisitor import *
+import sys
+
+def ical2cnode(ical):
+    r = CNode(name=ical.name)
+
+    for k,v in ical.iteritems():
+        r.attrs[k] = v
+
+    for child in ical.walk():
+        if child == ical: continue
+
+        cnode = ical2cnode(child)
+        if cnode.content != '':
+            r.add_child(cnode)
+
+    return r
+
+def xml2cnode(xml):
+    r = CNode(name=xml.tag)
+    r.content = xml.text
+
+    for k,v in xml.attrib.iteritems():
+        r.attrs[k] = v
+
+    for c in xml.getchildren():
+        e = xml2cnode(c)
+        r.add_child(e)
+
+    return r

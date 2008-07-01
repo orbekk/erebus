b485c2714b36f90bf40c7ad5db029535f4894c41
Stian Husemoen <stian.husemoen@batbelt.org>
2008-07-01 13:55:30 +0200

    fixed import path

--- utils/icalconv.py
+++ utils/icalconv.py
@@ -1,10 +1,16 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.extend(['.', '../'])
+
 from icalendar import Calendar as ICal
 from CNode2StringVisitor import *
 from cnodegen import *
 from CNode import *
 from ICS2ErebusVisitor import *
-import sys
+
+
+
     
 if len(sys.argv) != 2:
     print 'Usage: python %s <ical file>' % sys.argv[0]

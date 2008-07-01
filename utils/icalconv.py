efb28aedb5f610d233c85aeb5a55e27c20c3e993
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-01 13:11:51 +0200

    Added icalconv script to load an ical file into a CTree

---
+++ utils/icalconv.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+from icalendar import Calendar as ICal
+from CNode2StringVisitor import *
+from cnodegen import *
+from CNode import *
+from ICS2ErebusVisitor import *
+import sys
+    
+if len(sys.argv) != 2:
+    print 'Usage: python %s <ical file>' % sys.argv[0]
+    sys.exit()
+
+f = sys.argv[1]
+cont = open(f).read()
+c = ICal.from_string(cont)
+cnode = ical2cnode(c)
+
+visitor = ICS2ErebusVisitor(cnode)
+ecal = visitor.visit_start()
+print CNode2StringVisitor().visit(ecal)

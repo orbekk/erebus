f04b6ed051635e17e9c132637af1857cabdba071
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-08 08:34:48 +0200

    Visitor exports all Visitors

---
+++ Visitor/__init__.py
@@ -0,0 +1,4 @@
+from Erebus2EWSVisitor import Erebus2EWSVisitor
+from Erebus2ICSVisitor import Erebus2ICSVisitor
+from EWS2ErebusVisitor import EWS2ErebusVisitor
+from ICS2ErebusVisitor import ICS2ErebusVisitor

e7a7488a0ba6cb741e84322364ee925fd3732984
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-09 09:17:33 +0200

    Implemented the get* and delete* methods in ExchangeBackend

--- Backend/ExchangeBackend.py
+++ Backend/ExchangeBackend.py
@@ -24,8 +24,44 @@ class ExchangeBackend(Backend):
     def __conv(self,string):
         return xml2cnode(ET.XML(string))
 
+    def get_item(self,id):
+        if id.name == 'exchange_id':
+            changekey = id.attr['changekey']
+            itemid = id.attr['id']
+        else:
+            raise ValueError("Unknown item %s" % str(id))
+
+        item = self.query.get_item((itemid, changekey), extra_props=['item:Body'])
+        ctree = self.__conv(item)
+        return EWS2ErebusVisitor(ctree).run()
+
     def get_all_items(self):
         items = self.query.get_all_calendar_items()
         ctree = self.__conv(items)
         
         return EWS2ErebusVisitor(ctree).run()
+
+    def get_all_item_ids(self):
+        items = self.query.find_items('calendar', baseshape='IdOnly')
+        ctree = self.__conv(items)
+        return EWS2ErebusVisitor(ctree).run()
+
+    def delete_item(self,id):
+        """Delete one or more items
+
+        Searches for exchange_ids in `id' and deletes all occurences.
+        """
+        exchange_ids = id.search('exchange_id', all=True)
+
+        itemids = []
+        for e in exchange_ids:
+            itemid = e.attr['id']
+            chkey = e.attr['changekey']
+            itemids.append((itemid,chkey))
+                
+
+        self.query.delete_items(itemids)
+        
+
+    def delete_all_items(self):
+        self.delete_item(self.get_all_item_ids())

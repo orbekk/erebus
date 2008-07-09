3113b494accdc5c4578d77e5e3cbc9fa699bf4a7
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-09 11:05:22 +0200

    Added support for outputting types

--- Visitor/ToStringVisitor.py
+++ Visitor/ToStringVisitor.py
@@ -4,12 +4,18 @@ from CNode import *
 
 class ToStringVisitor(CNodeVisitor):
 
-    def __init__(self):
+    def __init__(self, with_types=False):
         self.str = ""
+        self.with_types = with_types
         self.indent = 0
 
     def visit_any(self, o, indent=0):
-        self.str += '    ' * indent + '{'+o.name+'}' + '\n'
+        if self.with_types:
+            displayname = o.name + ' ' + str(type(o.name))
+        else:
+            displayname = o.name
+
+        self.str += '    ' * indent + '{'+displayname+'}' + '\n'
         cont = o.content
 
         # usj og fysj
@@ -17,10 +23,16 @@ class ToStringVisitor(CNodeVisitor):
             cont = str(cont)
         if type(cont) == str:
             cont = unicode(cont,'utf8')
-        
+
+        if self.with_types:
+            cont = cont + " (: %s)" % str(type(o.content))
+
         self.str += u'    ' * indent + u'  '+cont+ u'\n'
 
         for k,v in o.attr.iteritems():
+            if self.with_types:
+                k = "(%s : %s)" % (k, str(type(k)))
+                v = v + " (: %s)" % str(type(v))
             self.str += '    ' * indent + " - %s: %s\n" %(k,v)
 
         [self.visit(c,indent=indent+1) for c in o.children]

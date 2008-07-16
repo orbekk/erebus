b8b1d060fdd08acb3f253f1d4f1257b33d936d05
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-16 10:18:24 +0200

    Handle serving of exchange.ics

--- EDAV/ExchangeHandler.py
+++ EDAV/ExchangeHandler.py
@@ -21,15 +21,39 @@ class ExchangeHandler(dav_interface):
         if self.verbose:
             print >>sys.stderr, '>> (DAVExchange) %s' % message
 
+    def _get_dav_getcontenttype(self,uri):
+        path = self.uri2local(uri)
+
+        content_types = {
+            '/info': 'text/plain',
+            '/calendar/exchange.ics': 'text/calendar'
+            }
+
+        if content_types.has_key(path):
+            return content_types[path]
+
+        raise DAV_NotFound, 'Could not find %s' % uri
+
     def get_data(self,uri):
         path = self.uri2local(uri)
 
+        if path == '/info':
+            return str(self.handler.headers['Authorization'])
+
         if path == '/calendar/exchange.ics':
-            b = ExchangeBackend(host=host,https=False,user=username,
-                                    password=password)
-            its = b.get_all_items()
-            ics = Erebus2ICSVisitor(its).run()
-            ics = cnode2ical(ics).as_string()
+            # Get auth string from handler
+            auth = ('Authorization', self.handler.headers['Authorization'])
+
+            try:
+                b = ExchangeBackend(host=host,https=False,auth=auth)
+                its = b.get_all_items()
+                ics = Erebus2ICSVisitor(its).run()
+                ics = cnode2ical(ics).as_string()
+            except QueryError, e:
+                if e.status == 401:
+                    self.handler.send_autherror(401,"Authorization Required")
+                    return
+                raise e
 
             return ics
 

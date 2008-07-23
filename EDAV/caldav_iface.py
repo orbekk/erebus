ab21c858442b693edae58ae230b1d5391f67e2a7
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-23 14:21:42 +0200

    Added caldav_interface

---
+++ EDAV/caldav_iface.py
@@ -0,0 +1,25 @@
+"""
+
+CalDAV interface class that extends DAV.dav_interface.
+
+Subclasses must implement the query_calendar method
+
+TODO: add extra properties from the RFC here
+"""
+
+class caldav_interface(dav_interface):
+
+    def query_calendar(self,uri,filter,calendar_data):
+        """This is the filter from a calendar-query
+
+        This method should return the calendar_data in `uri' after
+        applying the filter. Only elements in calendar_data should be
+        returned
+
+        uri: the calendar uri
+        filter: the CALDAV:filter element from the query
+        calendar_data: the CALDAV:calendar-data from the query
+        """
+        raise NotImplementedError(
+            'Please implement query_calendar in your subclass')
+    

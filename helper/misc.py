593859062037e0ff05688ff53c286ef9d4bffc15
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-29 09:44:31 +0200

    Actually returned the exchange_id node

--- helper/misc.py
+++ helper/misc.py
@@ -4,6 +4,7 @@ from CNode import *
 
 def create_exchange_id(eid, e_chkey):
     n = CNode('exchange_id')
-    n['id'] = eid
-    n['changekey'] = e_chkey
+    n.attr['id'] = eid
+    n.attr['changekey'] = e_chkey
+    return n
 

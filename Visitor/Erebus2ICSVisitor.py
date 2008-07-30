c200d1ebee373c99913400164723cea2442e32d8
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-07-30 12:57:18 +0200

    fixed Erebus2ICSVisitor bug

--- Visitor/Erebus2ICSVisitor.py
+++ Visitor/Erebus2ICSVisitor.py
@@ -14,16 +14,14 @@ class Erebus2ICSVisitor(CNodeVisitor):
 
     def __init__(self,cnode=None):
         self.ebus = cnode
-
-
-    def run(self, ebus=None):
-        if ebus:
-            self.ebus = ebus
-
         self.cal = CNode('vcalendar')
         
         self.cal.attr['prodid'] = '-//Erebus//hig.no//'
         self.cal.attr['version'] = '2.0'
+
+    def run(self, ebus=None):
+        if ebus:
+            self.ebus = ebus
 
         for tz in self.accept(self.ebus, 'TimeZone'):
             self.cal.add_child(tz)
